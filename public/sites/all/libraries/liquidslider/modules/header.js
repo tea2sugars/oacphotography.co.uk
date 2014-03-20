ddef5b7fdc3fb5a1a319ac513707ba1c7e7d4a62
Rik Barwick <rik@creativitea.co.uk>
2014-03-20 16:02:27 +0000

    drupal site

---
+++ public/sites/all/libraries/liquidslider/modules/header.js
@@ -0,0 +1,10 @@
+/*!
+<<<<<<< HEAD
+ *  Liquid Slider v2.0.7
+=======
+ *  Liquid Slider v2.0.8
+>>>>>>> origin/gh-pages
+ *  http://liquidslider.com
+ *  GPL license
+ */
+;if(typeof Object.create!=="function"){Object.create=function(b){function a(){}a.prototype=b;return new a()}}(function(d,c,a,e){var b={
---
+++ public/sites/all/themes/oac_photography_v1_1/templates/html.tpl.php
@@ -0,0 +1,19 @@
+<?php print $doctype; ?>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
+<head<?php print $rdf->profile; ?>>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>  
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <link href="http://localhost:8888/oac_drupal/public/sites/all/themes/oac_photography_v1_1/css/fonts.css" media="screen" rel="stylesheet" type="text/css" />
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body<?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
