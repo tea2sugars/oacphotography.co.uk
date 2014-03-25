cb7ec0d1b8b58d19e6f47e804acf2ced46b52034
Rik Barwick <rik@creativitea.co.uk>
2014-03-25 21:51:13 +0000

    logo, grid, press images, fonts for local dev, blocks on homepage

--- public/sites/all/themes/oac_photography_v1_1/templates/html.tpl.php
+++ public/sites/all/themes/oac_photography_v1_1/templates/html.tpl.php
@@ -5,7 +5,7 @@
   <title><?php print $head_title; ?></title>  
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <link href="http://localhost:8888/oac_drupal/public/sites/all/themes/oac_photography_v1_1/css/fonts.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="http://oac.theteapot.me.uk/sites/all/themes/oac_photography_v1_1/css/fonts.css" media="screen" rel="stylesheet" type="text/css" />
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body<?php print $attributes;?>>
